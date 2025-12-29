24d9ccde40230734d433d2f867c381e25059ccd6
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-29 13:32:27 +0530

    create functions for save employee buttons

--- js/Employee.js
+++ js/Employee.js
@@ -1,3 +1,11 @@
 //to write all functions
 
 
+
+function saveEmployee(){
+let name=$('#exampleFormControlInput2').val();
+let address=$('#exampleFormControlInput3').val();
+let number=$('#exampleFormControlInput4').val();
+
+console.log(name);
+}
