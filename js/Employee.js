440569087fa969004c705973cf6a6d9b78bc38bb
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-29 15:29:03 +0530

    create functions for get all  employee table

--- js/Employee.js
+++ js/Employee.js
@@ -1,78 +1,112 @@
-//to write all functions
+// to write all functions
 
+getAllEmployee();
 
-function saveEmployee(){
-let name=$('#exampleFormControlInput2').val();
-let address=$('#exampleFormControlInput3').val();
-let number=$('#exampleFormControlInput4').val();
+function saveEmployee() {
+    let name = $('#exampleFormControlInput2').val();
+    let address = $('#exampleFormControlInput3').val();
+    let number = $('#exampleFormControlInput4').val();
 
-
-$.ajax({
-method:"POST",
-contentType:"application/json",
-url:"http://localhost:8080/api/v1/employee/saveemployee",
-async:true,
-data:JSON.stringify({
- "empId": "",
-  "empName": name,
-  "empAddress": address,
-  "empMNumber": number
-
-}),
-success:function(data){
-alert("saved")
-},
-error:function(xhr,exception){
-alert("Error")
+    $.ajax({
+        method: "POST",
+        contentType: "application/json",
+        url: "http://localhost:8080/api/v1/employee/saveemployee",
+        async: true,
+        data: JSON.stringify({
+            "empId": "",
+            "empName": name,
+            "empAddress": address,
+            "empMNumber": number
+        }),
+        success: function (data) {
+            alert("saved");
+            getAllEmployee();
+        },
+        error: function (xhr, exception) {
+            alert("Error");
+        }
+    });
 }
-})
+
+/* update function */
+
+function updateEmployee() {
+    let empId = $('#exampleFormControlInput1').val();
+    let name = $('#exampleFormControlInput2').val();
+    let address = $('#exampleFormControlInput3').val();
+    let number = $('#exampleFormControlInput4').val();
+
+    $.ajax({
+        method: "PUT",
+        contentType: "application/json",
+        url: "http://localhost:8080/api/v1/employee/updateemployee",
+        async: true,
+        data: JSON.stringify({
+            "empId": empId,
+            "empName": name,
+            "empAddress": address,
+            "empMNumber": number
+        }),
+        success: function (data) {
+            alert("updated successfully");
+            getAllEmployee();
+        },
+        error: function (xhr, exception) {
+            alert("Error");
+        }
+    });
 }
 
-/*update function*/
+/* delete function */
 
-function saveEmployee(){
-let empId=$('#exampleFormControlInput1').val();
-let name=$('#exampleFormControlInput2').val();
-let address=$('#exampleFormControlInput3').val();
-let number=$('#exampleFormControlInput4').val();
+function deleteEmployee() {
+    let empId = $('#exampleFormControlInput1').val();
 
-
-$.ajax({
-method:"PUT",
-contentType:"application/json",
-url:"http://localhost:8080/api/v1/employee/updateemployee",
-async:true,
-data:JSON.stringify({
- "empId": empId,
-  "empName": name,
-  "empAddress": address,
-  "empMNumber": number
-
-}),
-success:function(data){
-alert("updated suceesfully")
-},
-error:function(xhr,exception){
-alert("Error")
+    $.ajax({
+        method: "DELETE",
+        contentType: "application/json",
+        url: "http://localhost:8080/api/v1/employee/deleteemployee/" + empId,
+        async: true,
+        success: function (data) {
+            alert("deleted successfully");
+            getAllEmployee();
+        },
+        error: function (xhr, exception) {
+            alert("Error");
+        }
+    });
 }
-})
 
-/*delete function*/
+/* get all function */
 
-function deleteEmployee(){
-let empId=$('#exampleFormControlInput1').val();
+function getAllEmployee() {
+    $.ajax({
+        method: "GET",
+        contentType: "application/json",
+        url: "http://localhost:8080/api/v1/employee/getAllemployees",
+        async: true,
+        success: function (data) {
+            if (data.code === "00") {
+                $('#empTable').empty();
+                for (let emp of data.content) {
+                    let empId = emp.empId;
+                    let name = emp.empName;
+                    let address = emp.empAddress;
+                    let number = emp.empMNumber;
 
-$.ajax({
-method:"DELETE",
-contentType:"application/json",
-url:"http://localhost:8080/api/v1/employee/deleteemployee/"+empId,
-async:true,
-success:function(data){
-alert("updated suceesfully")
-},
-error:function(xhr,exception){
-alert("Error")
+                    var row = `<tr>
+                        <td>${empId}</td>
+                        <td>${name}</td>
+                        <td>${address}</td>
+                        <td>${number}</td>
+                    </tr>`;
+
+                    $('#empTable').append(row);
+                }
+            }
+        },
+        error: function (xhr, exception) {
+            alert("Error");
+        }
+    });
 }
-})
-
-
