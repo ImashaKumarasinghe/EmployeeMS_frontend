028f126d55881f9d0e72591393e071aca8d6986e
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-29 15:05:39 +0530

    create functions for update employee buttons

--- js/Employee.js
+++ js/Employee.js
@@ -12,11 +12,11 @@ method:"POST",
 contentType:"application/json",
 url:"http://localhost:8080/api/v1/employee/saveemployee",
 async:true,
-data.JSON.stringify({
+data:JSON.stringify({
  "empId": "",
-  "empName": "name",
-  "empAddress": "address",
-  "empMNumber": "number"
+  "empName": name,
+  "empAddress": address,
+  "empMNumber": number
 
 }),
 success:function(data){
@@ -27,3 +27,32 @@ alert("Error")
 }
 })
 }
+
+/*update function*/
+
+function saveEmployee(){
+let empId=$('#exampleFormControlInput1').val();
+let name=$('#exampleFormControlInput2').val();
+let address=$('#exampleFormControlInput3').val();
+let number=$('#exampleFormControlInput4').val();
+
+
+$.ajax({
+method:"PUT",
+contentType:"application/json",
+url:"http://localhost:8080/api/v1/employee/updateemployee",
+async:true,
+data:JSON.stringify({
+ "empId": empId,
+  "empName": name,
+  "empAddress": address,
+  "empMNumber": number
+
+}),
+success:function(data){
+alert("updated suceesfully")
+},
+error:function(xhr,exception){
+alert("Error")
+}
+})
