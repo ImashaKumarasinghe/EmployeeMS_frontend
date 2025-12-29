be1f151115b2a731d0cf88ba592429687d304190
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-29 13:32:27 +0530

    create functions for save employee buttons

--- js/Employee.js
+++ js/Employee.js
@@ -1,3 +1,30 @@
 //to write all functions
 
 
+
+function saveEmployee(){
+let name=$('#exampleFormControlInput2').val();
+let address=$('#exampleFormControlInput3').val();
+let number=$('#exampleFormControlInput4').val();
+
+
+$.ajax({
+method:"POST",
+contentType:"application/json",
+url:"http://localhost:8080/api/v1/employee/saveemployee",
+async:true,
+data.JSON.stringify({
+ "empId": "",
+  "empName": "name",
+  "empAddress": "address",
+  "empMNumber": "number"
+
+}),
+success:function(data){
+alert("saved")
+},
+error:function(xhr,exception){
+alert("Error")
+}
+})
+}
